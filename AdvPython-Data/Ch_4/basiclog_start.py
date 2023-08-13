8d57ee9a2ae263f726b2a6de3d16740346ac8b32
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2023-08-13 22:38:13 +0530

    Add the Ch_4 basiclog soln

--- AdvPython-Data/Ch_4/basiclog_start.py
+++ AdvPython-Data/Ch_4/basiclog_start.py
@@ -1,11 +1,18 @@
 # demonstrate the logging api in Python
 
 # TODO: use the built-in logging module
-
+import logging
 
 # TODO: Use basicConfig to configure logging
-
+logging.basicConfig(
+    level=logging.DEBUG, filename="output.log"
+)  # All levels will be printed
 # TODO: Try out each of the log levels
+logging.debug("This is a debug message")
+logging.info("This is a info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
 
 # TODO: Output formatted strings to the log
-
+logging.debug("This is a formated string with x={} and y={}".format("ssg", 10))
