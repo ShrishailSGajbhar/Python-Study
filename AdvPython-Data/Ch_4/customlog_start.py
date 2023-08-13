dcc347d80a91f72911bc47f02b66397d2be3ff97
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2023-08-13 22:55:27 +0530

    Add the Ch_4 customlog soln

--- AdvPython-Data/Ch_4/customlog_start.py
+++ AdvPython-Data/Ch_4/customlog_start.py
@@ -2,14 +2,20 @@
 
 import logging
 
+extData = {'user': 'shrishailgajbhar@example.com'}
+
 # TODO: add another function to log from
+def another_function():
+    logging.debug("This is a log message from another fun", extra=extData)
 
 
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
-logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
+fmtStr = f"%(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d User:%(user)s %(message)s"
+dateStr = "%m/%d/%Y %I:%M:%S %p"
+logging.basicConfig(filename="output.log", level=logging.DEBUG,
+                    format=fmtStr, datefmt=dateStr)
 
-logging.info("This is an info-level log message")
-logging.warning("This is a warning-level message")
-
+logging.info("This is an info-level log message", extra=extData)
+logging.warning("This is a warning-level message", extra=extData)
+another_function()
