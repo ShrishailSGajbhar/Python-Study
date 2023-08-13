173b77c6f2d4368c33a738b1aa024a762c8a0d01
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2023-08-13 19:38:19 +0530

    Add the Ch_1 completed and collections datatypes

--- AdvPython-Data/Ch_2/namedtuple.py
+++ AdvPython-Data/Ch_2/namedtuple.py
@@ -4,5 +4,11 @@ import collections
 
 
 # TODO: create a Point namedtuple
-
+Point = collections.namedtuple("Point", "x y")
+p1 = Point(2,3)
+p2 = Point(5,6)
+print(p1, p2)
+print(p1.x, p1.y)
 # TODO: use _replace to create a new instance
+p1 = p1._replace(x=100)
+print(p1)
