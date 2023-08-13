88b2726238a26c5faede47663df757af0d2ed287
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2023-08-13 20:05:01 +0530

    Add the Ch_2 completed

--- AdvPython-Data/Ch_2/challenge_start.py
+++ AdvPython-Data/Ch_2/challenge_start.py
@@ -5,5 +5,14 @@ import json
 
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+from collections import defaultdict
+d = defaultdict(int)
+
+for feature in data['features']:
+    d[feature['properties']['type']]+=1
+
+for k,v in d.items():
+    print(f"{k}: {v}")
--- AdvPython-Data/Ch_2/deque.py
+++ AdvPython-Data/Ch_2/deque.py
@@ -5,11 +5,19 @@ import string
 
 
 # TODO: initialize a deque with lowercase letters
-
+d = collections.deque(string.ascii_lowercase)
 # TODO: deques support the len() function
-
+print(f"Item length = {len(d)}")
 # TODO: deques can be iterated over
-
+for elem in d:
+    print(elem.upper(), end=" ")
 # TODO: manipulate items from either end
-
+d.popleft()
+d.pop()
+print(d)
+d.append(1)
+d.appendleft(2)
+print(d)
 # TODO: use an index to get a particular item
+print(d[-1])
+print(d[0])
