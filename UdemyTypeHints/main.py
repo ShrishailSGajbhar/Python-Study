e2192827750ff62ffb72a83e23dc206eb635aaa6
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2023-09-14 22:11:16 +0530

    show use of hints in function

--- UdemyTypeHints/main.py
+++ UdemyTypeHints/main.py
@@ -3,3 +3,9 @@ text:str = "hello"
 number:int = 10
 percent:float = 0.50
 is_connected:bool = False
+
+# if we give type hints explicitly, it helps editor to provide hints
+def format_input(user_input:str):
+    return user_input.capitalize()
+
+print(format_input("hello world"))
